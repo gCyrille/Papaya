d4fea0f7fd7f30bc2a641652a4e6a648b2c2ae37
Cyrille G. <pro@cyrilleg.me>
2025-02-10 22:43:27 +0100

    update third parties

--- application/libraries/third_party/PhpOffice/PhpSpreadsheet/Reader/Xls/MD5.php
+++ application/libraries/third_party/PhpOffice/PhpSpreadsheet/Reader/Xls/MD5.php
@@ -24,7 +24,7 @@ class MD5
     /**
      * Reset the MD5 stream context.
      */
-    public function reset()
+    public function reset(): void
     {
         $this->a = 0x67452301;
         $this->b = 0xEFCDAB89;
@@ -56,7 +56,7 @@ class MD5
      *
      * @param string $data Data to add
      */
-    public function add($data)
+    public function add($data): void
     {
         $words = array_values(unpack('V16', $data));
 
@@ -65,10 +65,10 @@ class MD5
         $C = $this->c;
         $D = $this->d;
 
-        $F = ['self', 'f'];
-        $G = ['self', 'g'];
-        $H = ['self', 'h'];
-        $I = ['self', 'i'];
+        $F = [self::class, 'f'];
+        $G = [self::class, 'g'];
+        $H = [self::class, 'h'];
+        $I = [self::class, 'i'];
 
         // ROUND 1
         self::step($F, $A, $B, $C, $D, $words[0], 7, 0xd76aa478);
@@ -168,7 +168,7 @@ class MD5
         return $Y ^ ($X | (~$Z)); // Y XOR (X OR NOT Z)
     }
 
-    private static function step($func, &$A, $B, $C, $D, $M, $s, $t)
+    private static function step($func, &$A, $B, $C, $D, $M, $s, $t): void
     {
         $A = ($A + call_user_func($func, $B, $C, $D) + $M + $t) & 0xffffffff;
         $A = self::rotate($A, $s);
